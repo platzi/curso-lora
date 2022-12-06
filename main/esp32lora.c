805f222c3116db4a964191dc177f800aa9b3f18e
Diana Martinez <diananerdoficial@gmail.com>
2022-12-06 15:51:34 -0600

    Hello World base

--- main/esp32lora.c
+++ main/esp32lora.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "ssd1306.h"
 
-void app_main(void)
-{
+SSD1306_t screen;
 
+void app_main(void) {
+  i2c_master_init(&screen, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
+  ssd1306_init(&screen, 128, 64);
+  ssd1306_clear_screen(&screen, false);
+  ssd1306_contrast(&screen, 0xFF);
+  ssd1306_clear_line(&screen, 0, false);
+  ssd1306_display_text(&screen, 0, "Hola mundo!", 11, false);
 }
