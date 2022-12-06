c357203407b90b5d136b2867564890d4a1befb48
Diana Martinez <diananerdoficial@gmail.com>
2022-12-06 16:22:37 -0600

    Refactor to functions

--- main/esp32lora.c
+++ main/esp32lora.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include "string.h"
 #include "ssd1306.h"
 
 SSD1306_t screen;
 
-void app_main(void) {
+void screen_init() {
   i2c_master_init(&screen, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
   ssd1306_init(&screen, 128, 64);
-  ssd1306_clear_screen(&screen, false);
   ssd1306_contrast(&screen, 0xFF);
-  ssd1306_clear_line(&screen, 0, false);
-  ssd1306_display_text(&screen, 0, "Hola mundo!", 11, false);
+}
+
+void screen_clear() {
+  ssd1306_clear_screen(&screen, false);
+}
+
+void screen_print(char * str, int page) {
+  ssd1306_clear_line(&screen, page, false);
+  ssd1306_display_text(&screen, page, str, strlen(str), false);
+}
+
+void app_main(void) {
+  screen_init();
+  screen_clear();
+  screen_print("Hola mundo!", 0);
 }
