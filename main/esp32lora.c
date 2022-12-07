c93dba0ea7766ccfbf846436cfec906bbb397371
Diana Martinez <diananerdoficial@gmail.com>
2022-12-06 18:47:39 -0600

    Unstable version

--- main/esp32lora.c
+++ main/esp32lora.c
@@ -8,6 +8,7 @@
 #define MESSAGE_LENGTH 240
 
 SSD1306_t screen;
+TaskHandle_t xHandleRXTask;
 uint8_t msg[MESSAGE_LENGTH];
 int packets = 0;
 int rssi = 0;
@@ -49,9 +50,18 @@ void task_rx(void *p) {
   }
 }
 
+void send_msg(char * msg, int size) {
+  printf("send packet: %s\n", msg);
+  vTaskSuspend(xHandleRXTask);
+  lora_send_packet((uint8_t *)msg, size);
+  vTaskResume(xHandleRXTask);
+  printf("packet sent!\n");
+}
+
 void task_tx(void *p) {
   for(;;) {
-    
+    send_msg("Ping!", 5);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -60,7 +70,7 @@ void lora_config_init() {
   lora_init();
   lora_set_frequency(915e6);
   lora_enable_crc();
-  xTaskCreate(&task_rx, "task_rx", 2048, NULL, 5, NULL);
+  xTaskCreate(&task_rx, "task_rx", 2048, NULL, 5, &xHandleRXTask);
   xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
 }
 
