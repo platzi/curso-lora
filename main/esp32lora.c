bd846047dc48e470c5af9c8741729d1288acd035
Diana Martinez <diananerdoficial@gmail.com>
2022-12-07 15:57:33 -0600

    Add WiFi and HTTP server

--- main/esp32lora.c
+++ main/esp32lora.c
@@ -4,8 +4,17 @@
 #include "freertos/task.h"
 #include "ssd1306.h"
 #include "lora.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_http_server.h"
 
 #define MESSAGE_LENGTH 240
+
+extern const char index_start[] asm("_binary_index_html_start");
+extern const char index_end[] asm("_binary_index_html_end");
+extern const char logo_start[] asm("_binary_logo_png_start");
+extern const char logo_end[] asm("_binary_logo_png_end");
 
 SSD1306_t screen;
 TaskHandle_t xHandleRXTask;
@@ -45,8 +54,8 @@ void task_rx(void *p) {
 
       sprintf(packets_count, "Count: %d", packets);
       sprintf(rssi_str, "RSSI: %d dBm", rssi);
-      screen_print(packets_count, 0);
-      screen_print(rssi_str, 1);
+      screen_print(packets_count, 6);
+      screen_print(rssi_str, 4);
     }
     vTaskDelay(1);
   }
@@ -76,9 +85,88 @@ void lora_config_init() {
   xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
 }
 
+static esp_err_t home_get_handler(httpd_req_t *req) {
+  httpd_resp_set_type(req, "text/html");
+
+  const uint32_t index_len = index_end - index_start;
+  httpd_resp_send(req, index_start, index_len);
+
+  return ESP_OK;
+}
+
+static esp_err_t logo_get_handler(httpd_req_t *req) {
+  httpd_resp_set_type(req, "image/png");
+
+  const uint32_t logo_len = logo_end - logo_start;
+  httpd_resp_send(req, logo_start, logo_len);
+
+  return ESP_OK;
+}
+
+static const httpd_uri_t home = {
+  .uri = "/",
+  .method = HTTP_GET,
+  .handler = home_get_handler
+};
+
+static const httpd_uri_t logo = {
+  .uri = "/logo.png",
+  .method = HTTP_GET,
+  .handler = logo_get_handler
+};
+
+void web_server_init() {
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+  if (httpd_start(&server, &config) == ESP_OK) {
+    httpd_register_uri_handler(server, &home);
+    httpd_register_uri_handler(server, &logo);
+    return;
+  }
+
+  printf("Error al iniciar servidor\n");
+}
+
+
 void app_main(void) {
   screen_init();
   screen_clear();
+
   // screen_print("Hola mundo!", 0);
   lora_config_init();
+
+  nvs_flash_init();
+  esp_netif_init();
+  esp_event_loop_create_default();
+  example_connect();
+
+  esp_netif_ip_info_t ip_info;
+  esp_netif_t *netif = NULL;
+  netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
+
+  if (netif == NULL) {
+    printf("No hay interfaz\n");
+  } else {
+    wifi_config_t conf;
+
+    if (esp_wifi_get_config(WIFI_IF_STA, &conf) == ESP_OK) {
+      printf("WiFi: %s\n", (char*)conf.sta.ssid);
+      char ssid[64];
+      sprintf(ssid, "%s\n", (char *)conf.sta.ssid);
+      screen_print(ssid, 0);
+    }
+
+    if (esp_netif_get_ip_info(netif, &ip_info) == ESP_OK) {
+      printf("IP: %d.%d.%d.%d\n", IP2STR(&ip_info.ip)); 
+      char ip[64];
+      sprintf(ip, "%d.%d.%d.%d\n", IP2STR(&ip_info.ip));
+      screen_print(ip, 2);
+    }
+
+    screen_print("RSSI: -- dBm", 4);
+    screen_print("Count: 0", 6);
+
+    web_server_init();
+  }
 }
